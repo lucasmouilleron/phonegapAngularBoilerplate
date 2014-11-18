f5ec0922394cd6b180955e180ee65c7adf47df2d
Lucas Mouilleron <LucasMouileron>
2014-11-18 18:46:38 +0100

    first

---
+++ README.md
@@ -0,0 +1,36 @@
+phonegapAngularBoilerplate
+==========================
+
+Install requirements
+--------------------
+- Install NodeJS : http://nodejs.org/download
+- `sudo npm install -g phonegap`
+- `sudo npm install bower -g`
+- `sudo npm install grunt -g`
+- `sudo gem install sass`
+- `sudo gem install --pre sass-css-importer`
+
+Install
+-------
+- `cd _build && npm install`
+- iOS : 
+    - `npm install -g ios-deploy && npm install -g ios-sim`
+    - `phonegap run ios`
+- Android : 
+    - Download the Android SDK : https://developer.android.com/sdk
+    - Edit path : 
+        - `export ANDROID_HOME=$HOME/Library/Android`
+        - `export PATH=$PATH:$ANDROID_HOME/tools`
+    - Launch android sdk manager : `android`
+    - Install packages (SDK + images)
+    - Tools -> Manage AVDs and create a device (make sure the SDK is )
+    - `phonegap run android`
+
+Plugins
+-------
+- http://docs.phonegap.com/en/3.5.0/cordova_plugins_pluginapis.md.html#Plugin%20APIs
+- `phonegap plugin add org.apache.cordova.device`
+
+Miscs
+-----
+- scaffholder : `phonegap create . com.lucasmouilleron.phonegapAngularBoilerplate phonegapAngularBoilerplate`
---
+++ www/js/app.js
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////
+// App
+/////////////////////////////////////////////////////////////////////
+define(["angular", "filters/index", "services/index", "directives/index", "controllers/index", "angularRoute", "angularAnimate", "angularLoadBar", "angularGrowl", "angularDialog", "angularSweetAlert"], function (angular, filters, services, directives, controllers) {
+
+    /////////////////////////////////////////////////////////////////////
+    var app = angular.module("app", ["app.filters","app.services","app.directives","app.controllers", "ngRoute", "ngAnimate", "angular-loading-bar", "angular-growl", "ngDialog","oitozero.ngSweetAlert"]);
+
+    /////////////////////////////////////////////////////////////////////
+    app.config(["growlProvider", function(growlProvider) {
+        growlProvider.globalTimeToLive(5000);
+        growlProvider.globalDisableCountDown(true);
+        growlProvider.globalPosition("bottom-right");
+        growlProvider.onlyUniqueMessages(false);
+    }]);
+    
+    /////////////////////////////////////////////////////////////////////
+    app.config(["$locationProvider", function($location) {
+        $location.hashPrefix("!");
+    }]);
+
+    /////////////////////////////////////////////////////////////////////
+    app.run(["$rootScope", "$timeout", "$window", "Device", function ($rootScope, $timeout, $window, Device) {
+        
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {
+            Device.set(device);
+        }
+
+        $rootScope.$on('$routeChangeSuccess', function () {
+            $timeout(function () {
+                $window.scrollTo(0,0);
+            }, 500);
+        });
+    }]);
+
+    return app;
+
+});
